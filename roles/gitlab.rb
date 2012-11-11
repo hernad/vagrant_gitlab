83fd67c637d9d10c77e50efcf2c52bcbf4a7b646
Ernad Husremovic <hernad@bring.out.ba>
2012-11-11 13:47:00 +0100

    MYSQL envars

--- roles/gitlab.rb
+++ roles/gitlab.rb
@@ -10,9 +10,15 @@ run_list "recipe[build-essential]", "recipe[rvm::system]",
 #default_attributes "apache2" => { "listen_ports" => [ "80", "443" ] }
 #override_attributes "apache2" => { "max_children" => "50" }   
 
-if ENV['GMAIL_PASSWORD'].nil? or ENV['GMAIL_USER'].nil?
-    puts "morate definisati GMAIL_PASSWORD = #{ENV['GMAIL_PASSWORD']} i GMAIL_USER = #{ENV['GMAIL_USER']} environment varijable koje ce koristiti postfix !"
-    exit 1
+envars = %{GMAIL_PASSWORD GMAIL_USER MYSQL_ROOT_PWD MYSQL_PWD}
+envars = envars.split(" ")
+
+envars_defined = true
+envars.each { || ENV[e].nil? ? envars_defined = false : nil }
+
+if not envars_defined
+   puts "morate definisati OS envars #{envars.join(' ') }"
+   exit 1
 end
 
 override_attributes(
@@ -43,20 +49,13 @@ override_attributes(
            "smtp_sasl_user_name" => ENV['GMAIL_USER'],
            "smtp_sasl_passwd" => ENV['GMAIL_PASSWORD']
       },
-      :mysql => { "server_root_password" => "rootpwd" },
+      :mysql => { "server_root_password" => ENV['MYSQL_ROOT_PWD'] },
       :gitlab => {
-        "site" => "gitlab.test.out.ba",
+        "site" => ENV['OS_SERVER_NAME'],
         "https"  => true,
         "project_limit" => "20",
-        "email" => "bakir.husremovic@bring.out.ba", 
+        "email" => ENV['GMAIL_USER'], 
         "use_ldap" => false,
-            #host: '_your_ldap_server'
-            #base: '_the_base_where_you_search_for_users'
-            #port: 636
-            #uid: 'sAMAccountName'
-            #method: 'ssl' / plain
-            #bind_dn: '_the_full_dn_of_the_user_you_will_bind_with'
-            #password: '_the_password_of_the_bind_user'
         "git_max_size" => 5242880,  #5 megabytes,
         "user" => "www-data",
         "group" => "www-data",
@@ -68,11 +67,11 @@ override_attributes(
         "rvm_gemset" => "gitlab",
          "db" => {
            "rails_adapter" => "mysql2", #ruby 1.9 expects mysql2
-           "db_name" => "gitlab_production",
+           "db_name" => "gitlab",
            "db_name_test" => "gitlab_test",
            "db_name_development" => "gitlab_development",
            "db_user" => "gitlab",
-           "db_pass" => "pwd",
+           "db_pass" => ENV['MYSQL_PWD'],
            "db_host" => "localhost",
            "load_sql_file" => nil
          }
